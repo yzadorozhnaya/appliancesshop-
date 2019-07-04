198d79158655a7ae732b094e2d28e6d3ff5ac8b6
Katya Zadorozhnaya <katya.zadorognay@gmail.com>
2019-07-04 18:18:30 +0300

    asset()

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -3,34 +3,34 @@
 
 <head>
   <!-- Modernizer js -->
-    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="{{asset("js/vendor/modernizr-3.5.0.min.js")}}"></script>
 </head>
 <body>
 <!-- jquery 3.2.1 -->
-    <script src="js/vendor/jquery-3.2.1.min.js"></script>
+    <script src="{{asset("js/vendor/jquery-3.2.1.min.js")}}"></script>
     <!-- Countdown js -->
-    <script src="js/jquery.countdown.min.js"></script>
+    <script src="{{asset("js/jquery.countdown.min.js")}}"></script>
     <!-- Mobile menu js -->
-    <script src="js/jquery.meanmenu.min.js"></script>
+    <script src="{{asset("js/jquery.meanmenu.min.js")}}"></script>
     <!-- ScrollUp js -->
-    <script src="js/jquery.scrollUp.js"></script>
+    <script src="{{asset("js/jquery.scrollUp.js")}}"></script>
     <!-- Nivo slider js -->
-    <script src="js/jquery.nivo.slider.js"></script>
+    <script src="{{asset("js/jquery.nivo.slider.js")}}"></script>
     <!-- Fancybox js -->
-    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="{{asset("js/jquery.fancybox.min.js")}}"></script>
     <!-- Jquery nice select js -->
-    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="{{asset("js/jquery.nice-select.min.js")}}"></script>
     <!-- Jquery ui price slider js -->
-    <script src="js/jquery-ui.min.js"></script>
+    <script src="{{asset("js/jquery-ui.min.js")}}"></script>
     <!-- Owl carousel -->
-    <script src="js/owl.carousel.min.js"></script>
+    <script src="{{asset("js/owl.carousel.min.js")}}"></script>
     <!-- Bootstrap popper js -->
-    <script src="js/popper.min.js"></script>
+    <script src="{{asset("js/popper.min.js")}}"></script>
     <!-- Bootstrap js -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="{{asset("js/bootstrap.min.js")}}"></script>
     <!-- Plugin js -->
-    <script src="js/plugins.js"></script>
+    <script src="{{asset("js/plugins.js")}}"></script>
     <!-- Main activaion js -->
-    <script src="js/main.js"></script>
+    <script src="{{asset("js/main.js")}}"></script>
 </body>
 </html>
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,33 +1,33 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <!-- Favicons -->
-    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="shortcut icon" href="{{asset("img/favicon.ico")}}">
     <!-- Fontawesome css -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset("/css/font-awesome.min.css")}}">
     <!-- Ionicons css -->
-    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="{{asset("/css/ionicons.min.css")}}">
     <!-- linearicons css -->
-    <link rel="stylesheet" href="css/linearicons.css">
+    <link rel="stylesheet" href="{{asset("/css/linearicons.css")}}">
     <!-- Nice select css -->
-    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="{{asset("/css/nice-select.css")}}">
     <!-- Jquery fancybox css -->
-    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" href="{{asset("/css/jquery.fancybox.css")}}">
     <!-- Jquery ui price slider css -->
-    <link rel="stylesheet" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" href="{{asset("/css/jquery-ui.min.css")}}">
     <!-- Meanmenu css -->
-    <link rel="stylesheet" href="css/meanmenu.min.css">
+    <link rel="stylesheet" href="{{asset("/css/meanmenu.min.css")}}">
     <!-- Nivo slider css -->
-    <link rel="stylesheet" href="css/nivo-slider.css">
+    <link rel="stylesheet" href="{{asset("/css/nivo-slider.css")}}">
     <!-- Owl carousel css -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset("/css/owl.carousel.min.css")}}">
     <!-- Bootstrap css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset("/css/bootstrap.min.css")}}">
     <!-- Custom css -->
-    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="{{asset("/css/default.css")}}">
     <!-- Main css -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{asset("/css/style.css")}}">
     <!-- Responsive css -->
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="{{asset("/css/responsive.css")}}">
 
    
 
